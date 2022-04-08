e9c10517af38b2aa88128a5bcc481c40c2fca670
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-04-08 17:45:05 +0600

    ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+/*page loader function*/
+var headLoader = document.querySelector('.loader-screen');
+window.onload = ()=> {
+  headLoader.style.display = 'none';
+}
+
+
+
+
+
+/*    Toggle menu open function    */
 var menuIco = document.querySelector('.menu-icon');
 var nav = document.querySelector('nav');
 var pageLinks = document.querySelector('.page-links') 
